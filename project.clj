b62f8a9714c8719a5fb9642e1664caff9046b1c6
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-08-11 18:42:12 -0700

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/http "0.1.0"
+(defproject macchiato/http "0.1.1"
   :description "Http request/response helpers for Macchiato."
   :url "https://github.com/macchiato-framework/macchiato-http"
   :scm {:name "git"
